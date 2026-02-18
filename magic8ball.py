1e8001aed324c570a9ecea785f44dba8207191d2
sk1856 <deathdevil016@gmail.com>
2026-02-18 18:13:16 +0530

    Add comments to magic8ball.py
    This for fun activity

---
+++ magic8ball.py
@@ -0,0 +1,22 @@
+import random #to import random things
+Question = input("Ask mew a question: ") # asking to write a question
+a = random.randint(1,9)
+if a ==1 :
+    print("yes definently")
+elif a==2 :
+    print("It is decidedly so")
+elif a==3 :
+    print("Without a doubt. ")
+elif a==4 :
+    print("Reply hazy, try again.")
+elif a==5:
+    print("Ask again later.")
+elif a==6 :
+    print("Better not tell you now.")
+elif a==7 :
+    print("My sources say no.")
+elif a==8 :
+   print("Outlook not so good.")
+elif a==9 :
+   print("Very doubtful.")    
+
